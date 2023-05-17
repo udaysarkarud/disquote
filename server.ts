1fec4f1df0e32a800f6ecc6cc3f091b05908af0e
udaysarkarud <udaysarkarud@gmail.com>
2023-05-17 20:53:14 +0600

    bot connected with server

--- server.ts
+++ server.ts
@@ -1,7 +1,27 @@
 import express from "express";
-
+import * as dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+dotenv.config();
 const app = express();
 const port = 8000;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.on("messageCreate", async function (message) {
+  try {
+    if (message.author.bot) return;
+    console.log(message.content);
+    message.reply(`You said: ${message.content}`);
+  } catch (err) {}
+});
+
+client.login(process.env.DISCORD_TOKEN);
+console.log("Its online");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
